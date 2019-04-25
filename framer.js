dff4d6ee5728f26208a17874216fba38fc49ae1b
evblance <evblance@users.noreply.github.com>
2019-04-26 09:21:04 +1000

    Controls border colour via change event on colour control

--- framer.js
+++ framer.js
@@ -1,10 +1,17 @@
 
 const image = document.getElementById('img'); 
 const borderControl = document.getElementById('borderControl');
+const borderColourControl = document.getElementById('borderColourControl');
 
 const updateBorderThickness = (event) => {
   const thickness = event.target.value;
-  image.style.border = `${thickness}px solid black`;
+  image.style.borderWidth = `${thickness}px`;
+};
+
+const updateBorderColour = (event) => {
+  const colour = event.target.value;
+  image.style.borderColor = colour;
 };
 
 borderControl.addEventListener('mousemove', updateBorderThickness);
+borderColourControl.addEventListener('change', updateBorderColour);
