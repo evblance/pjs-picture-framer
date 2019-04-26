b9765df9c4becf42b9fb4866a5fcb3855b338f6c
evblance <evblance@users.noreply.github.com>
2019-04-26 10:06:15 +1000

    Documents event callback functions

--- framer.js
+++ framer.js
@@ -4,16 +4,28 @@ const borderControl = document.getElementById('borderControl');
 const borderColourControl = document.getElementById('borderColourControl');
 const blurControl = document.getElementById('blurControl');
 
+/**
+ * Updates the image border thickness.
+ * @param {any} event The border control input event.
+ */
 const updateBorderThickness = (event) => {
   const thickness = event.target.value;
   image.style.borderWidth = `${thickness}px`;
 };
 
+/**
+ * Updates the image border colour.
+ * @param {any} event The border colour input event.
+ */
 const updateBorderColour = (event) => {
   const colour = event.target.value;
   image.style.borderColor = colour;
 };
 
+/**
+ * Updates the image blur filter.
+ * @param {any} event The blur control input event.
+ */
 const updateBlur = (event) => {
   const blur = event.target.value;
   image.style.filter = `blur(${blur}px)`;
